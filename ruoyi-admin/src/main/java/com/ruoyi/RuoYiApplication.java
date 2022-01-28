a04646a8631ee1e57a103a0028478c825ad91675
zwgu <zwgu@travelsky.com.cn>
2022-01-28 13:56:21 +0800

    修改license模块

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,15 +1,8 @@
 package com.ruoyi;
 
-import com.ruoyi.common.constant.Constants;
-import com.ruoyi.common.enums.LicenseType;
-import com.ruoyi.common.utils.DateUtils;
-import com.ruoyi.common.utils.ip.IpUtils;
-import de.schlichtherle.license.LicenseContent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-import java.util.Date;
 
 /**
  * 启动程序
@@ -19,47 +12,8 @@ import java.util.Date;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RuoYiApplication {
     public static void main(String[] args) {
-
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
 //        RuoYiConfig config = SpringUtils.getBean(RuoYiConfig.class);
-
-        try {
-            LicenseContent license = Constants.LICENSE_CONTENT;
-            Date from = null;
-            Date to = null;
-            String licenceType = "未授权";
-            String appLimit = "未授权";
-            String maxOnline = "未授权";
-            String datetime = "0000-00-00 00:00:00 - 0000-00-00 00:00:00 (0天0小时0分钟)";
-            if (license != null) {
-                from = license.getNotBefore();
-                to = license.getNotAfter();
-                licenceType = LicenseType.valueOfCode(license.getConsumerType()).getInfo();
-                appLimit = "无限制";
-                maxOnline = String.valueOf(license.getConsumerAmount()) + " 人";
-                datetime = DateUtils.parseDateToStr(from) + " - " + DateUtils.parseDateToStr(to) +
-                        " (" + DateUtils.getDatePoor(to, from) + ")";
-            }
-
-            System.out.format(
-                    "===============================================================================================\n" +
-                            "//                                        服务器特征信息                                          \n" +
-                            "===============================================================================================\n" +
-                            "//              机　器　码: %-40.40s \n" +
-                            "//              服务器名称: %-24.24s 服务器IP: %-24.24s \n" +
-                            "===============================================================================================\n" +
-                            "//                                        服务器授权信息                                          \n" +
-                            "===============================================================================================\n" +
-                            "//              授权类型: %-12.12s 软件数量限制: %-12.12s 最大同时在线: %-12.12s \n" +
-                            "//              授权有效期限: %-80.80s \n" +
-                            "===============================================================================================\n" +
-                            ">>: 系统启动成功\n",
-                    Constants.SERVER_SN, IpUtils.getHostName(), IpUtils.getHostIp(),
-                    licenceType, appLimit, maxOnline, datetime);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(">>: 系统启动失败\n");
-        }
     }
 }
--- ruoyi-framework/src/main/java/com/ruoyi/framework/license/LicenseCheckListener.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/license/LicenseCheckListener.java
@@ -1,17 +1,25 @@
 package com.ruoyi.framework.license;
 
 import com.ruoyi.common.constant.Constants;
+import com.ruoyi.common.enums.LicenseType;
 import com.ruoyi.common.license.ServerInfo;
+import com.ruoyi.common.utils.DateUtils;
 import com.ruoyi.common.utils.PathUtils;
+import com.ruoyi.common.utils.ip.IpUtils;
 import com.ruoyi.framework.license.bo.LicenseVerifyParam;
+import de.schlichtherle.license.LicenseContent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 /**
  * 在项目启动时安装证书
@@ -57,9 +65,40 @@ public class LicenseCheckListener implements ApplicationListener<ContextRefreshe
             try {
                 licenseVerify.install(param);
                 log.info("License载入成功");
+
             } catch (Exception e) {
                 log.info("License载入失败：{}", e.getMessage());
             }
+        }
+        showLicenceInfo();
+    }
+
+    private void showLicenceInfo() {
+        try {
+            LicenseContent license = Constants.LICENSE_CONTENT;
+            Date from = null;
+            Date to = null;
+            String licenceType = "未授权";
+            String appLimit = "未授权";
+            String maxOnline = "未授权";
+            String datetime = "0000-00-00 00:00:00 - 0000-00-00 00:00:00 (0天0小时0分钟)";
+            if (license != null) {
+                from = license.getNotBefore();
+                to = license.getNotAfter();
+                licenceType = LicenseType.valueOfCode(license.getConsumerType()).getInfo();
+                appLimit = "无限制";
+                maxOnline = license.getConsumerAmount() + " 人";
+                datetime = DateUtils.parseDateToStr(from) + " - " + DateUtils.parseDateToStr(to) +
+                        " (" + DateUtils.getDatePoor(to, from) + ")";
+            }
+            File tipFile = PathUtils.getResourceFile("licenseTip.txt");
+            assert tipFile != null;
+            String tip = FileUtils.readFileToString(tipFile, StandardCharsets.UTF_8);
+            System.out.format(tip + "\n>>: 系统启动成功\n", Constants.SERVER_SN, IpUtils.getHostName(),
+                    IpUtils.getHostIp(), licenceType, appLimit, maxOnline, datetime);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("\n>>: 系统启动失败\n");
         }
     }
 }
