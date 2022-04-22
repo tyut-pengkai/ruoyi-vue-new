6da7ff4f5d2e8ba6efc5036e7762a81ad3408721
zwgu <1179403448@qq.com>
2022-04-22 22:11:30 +0800

    合并更新

---
+++ ruoyi-ui/src/views/index_bak2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <sale-view></sale-view>
+  </div>
+</template>
+
+<script>
+import SaleView from "@/views/system/dashboard/sale";
+
+export default {
+  name: "Index",
+  components: {
+    SaleView,
+  },
+  data() {
+    return {};
+  },
+  created() {
+  },
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
