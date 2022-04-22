7c56d5888d07df25ee1f7213a3825503fb68669b
zwgu <1179403448@qq.com>
2022-04-22 22:06:52 +0800

    合并更新

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -1,26 +1,218 @@
 <template>
-  <div>
-    <sale-view></sale-view>
+  <div class="app-container home">
+    <el-row :gutter="20">
+      <el-col :lg="12" :sm="24" style="padding-left: 20px">
+        <h2>INAMS软件管理系统</h2>
+        <p>软件授权管理计费销售一站式解决方案</p>
+        <p>
+          <b>当前版本:</b> <span>{{ version }}</span>
+        </p>
+        <!-- <p>
+          <el-tag type="danger">&yen; 限时免费</el-tag>
+        </p> -->
+        <!-- <p>
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-cloudy"
+            plain
+            @click="goTarget('https://gitee.com/y_project/RuoYi-Vue')"
+            >访问码云</el-button
+          >
+          <el-button
+            size="mini"
+            icon="el-icon-s-home"
+            plain
+            @click="goTarget('http://ruoyi.vip')"
+            >访问主页</el-button
+          >
+        </p> -->
+      </el-col>
+
+      <!-- <el-col :sm="24" :lg="12" style="padding-left: 50px">
+        <el-row>
+          <el-col :span="12">
+            <h2>技术选型</h2>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="6">
+            <h4>后端技术</h4>
+            <ul>
+              <li>SpringBoot</li>
+              <li>Spring Security</li>
+              <li>JWT</li>
+              <li>MyBatis</li>
+              <li>Druid</li>
+              <li>Fastjson</li>
+              <li>...</li>
+            </ul>
+          </el-col>
+          <el-col :span="6">
+            <h4>前端技术</h4>
+            <ul>
+              <li>Vue</li>
+              <li>Vuex</li>
+              <li>Element-ui</li>
+              <li>Axios</li>
+              <li>Sass</li>
+              <li>Quill</li>
+              <li>...</li>
+            </ul>
+          </el-col>
+        </el-row>
+      </el-col> -->
+    </el-row>
+    <el-divider/>
+    <el-row :gutter="20">
+      <el-col :lg="12" :md="12" :sm="24" :xs="24">
+        <el-card class="update-log">
+          <div slot="header" class="clearfix">
+            <span>联系信息</span>
+          </div>
+          <div class="body">
+            <!-- <p>
+              <i class="el-icon-s-promotion"></i> 官网：<el-link
+                href="http://www.ruoyi.vip"
+                target="_blank"
+                >http://www.ruoyi.vip</el-link
+              >
+            </p> -->
+            <p>
+              <i class="el-icon-user-solid"></i> QQ群：
+              <!-- <s>满937441</s> -->
+              <a href="https://jq.qq.com/?_wv=1027&k=3VxLmWXg" target="_blank"
+              >947144396</a
+              >
+            </p>
+            <p>
+              <i class="el-icon-chat-dot-round"></i> 微信：<a
+              href="javascript:;"
+            >coordsoft</a
+            >
+            </p>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20" style="margin-top: 20px">
+      <el-col :lg="12" :md="12" :sm="24" :xs="24">
+        <el-card class="update-log">
+          <div slot="header" class="clearfix">
+            <span>更新日志</span>
+          </div>
+          <el-collapse accordion>
+            <el-collapse-item title="v1.0.0 - 2022-05-01">
+              <ol>
+                <li>系统内测版v1.0完成</li>
+              </ol>
+            </el-collapse-item>
+          </el-collapse>
+        </el-card>
+      </el-col>
+      <!-- <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="update-log">
+          <div slot="header" class="clearfix">
+            <span>捐赠支持</span>
+          </div>
+          <div class="body">
+            <img
+              src="https://oscimg.oschina.net/oscnet/up-d6695f82666e5018f715c41cb7ee60d3b73.png"
+              alt="donate"
+              width="100%"
+            />
+            <span
+              style="display: inline-block; height: 30px; line-height: 30px"
+            >
+              您可以请作者喝杯咖啡表示鼓励
+            </span>
+          </div>
+        </el-card>
+      </el-col> -->
+    </el-row>
   </div>
 </template>
 
 <script>
-import SaleView from "@/views/system/dashboard/sale";
-
 export default {
   name: "Index",
-  components: {
-    SaleView,
-  },
   data() {
-    return {};
+    return {
+      // 版本号
+      version: "内测版v1.0",
+    };
   },
-  created() {
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    },
   },
-  methods: {},
-  computed: {},
 };
 </script>
 
 <style lang="scss" scoped>
+.home {
+  blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px solid #eee;
+  }
+
+  hr {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+
+  .col-item {
+    margin-bottom: 20px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 13px;
+  color: #676a6c;
+  overflow-x: hidden;
+
+  ul {
+    list-style-type: none;
+  }
+
+  h4 {
+    margin-top: 0px;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-size: 26px;
+    font-weight: 100;
+  }
+
+  p {
+    margin-top: 10px;
+
+    b {
+      font-weight: 700;
+    }
+  }
+
+  .update-log {
+    ol {
+      display: block;
+      list-style-type: decimal;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0;
+      margin-inline-end: 0;
+      padding-inline-start: 40px;
+    }
+  }
+}
 </style>
+
