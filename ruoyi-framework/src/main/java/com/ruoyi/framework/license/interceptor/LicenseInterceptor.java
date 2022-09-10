b900efcc691501050339699ed39d4f373035206d
zwgu <1179403448@qq.com>
2022-09-10 21:59:42 +0800

    优化：添加或更换授权文件后重新登录后台即可，无需再重启服务器

--- ruoyi-framework/src/main/java/com/ruoyi/framework/license/interceptor/LicenseInterceptor.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/license/interceptor/LicenseInterceptor.java
@@ -3,12 +3,14 @@ package com.ruoyi.framework.license.interceptor;
 import com.alibaba.fastjson.JSONObject;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.utils.ServletUtils;
+import com.ruoyi.framework.license.LicenseCheckListener;
 import com.ruoyi.framework.license.LicenseVerify;
 import com.ruoyi.framework.license.anno.LicenceCheck;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
@@ -20,6 +22,10 @@ import java.lang.reflect.Method;
  */
 @Component
 public class LicenseInterceptor implements HandlerInterceptor {
+
+    @Resource
+    private LicenseCheckListener licenseCheckListener;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (handler instanceof HandlerMethod) {
@@ -27,8 +33,10 @@ public class LicenseInterceptor implements HandlerInterceptor {
             Method method = handlerMethod.getMethod();
             LicenceCheck annotation = method.getAnnotation(LicenceCheck.class);
             if (annotation != null && annotation.value()) {
-                LicenseVerify licenseVerify = new LicenseVerify();
+                // 重载授权证书
+                licenseCheckListener.loadLicense();
                 //校验证书是否有效
+                LicenseVerify licenseVerify = new LicenseVerify();
                 if (!licenseVerify.verify()) {
                     AjaxResult ajaxResult = AjaxResult.error("您的授权证书无效，请核查服务器是否取得授权或重新申请证书！");
                     ServletUtils.renderString(response, JSONObject.toJSONString(ajaxResult));
