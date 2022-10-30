fdcd7ef06419bc0a60e71c67d38a054fe6bd3398
zwgu <1179403448@qq.com>
2022-10-31 00:45:24 +0800

    仪表盘统计显示NaN的问题

--- ruoyi-system/src/main/java/com/ruoyi/sale/mapper/DashboardSaleViewMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/sale/mapper/DashboardSaleViewMapper.java
@@ -17,13 +17,13 @@ import java.util.Map;
 @Repository
 public interface DashboardSaleViewMapper {
 
-    @Select("select sum(actual_fee) from sys_sale_order sso where sso.status in ('1', '3', '4')")
+    @Select("select IFNULL(sum(actual_fee),0) from sys_sale_order sso where sso.status in ('1', '3', '4')")
     public BigDecimal queryTotalFee();
 
-    @Select("select sum(actual_fee) from sys_sale_order sso where create_time BETWEEN #{start} and #{end}")
+    @Select("select IFNULL(sum(actual_fee),0) from sys_sale_order sso where create_time BETWEEN #{start} and #{end}")
     public BigDecimal queryTotalFeeAllBetween(@Param("start") String start, @Param("end") String end);
 
-    @Select("select sum(actual_fee) from sys_sale_order sso where sso.status in ('1', '3', '4') and create_time BETWEEN #{start} and #{end}")
+    @Select("select IFNULL(sum(actual_fee),0) from sys_sale_order sso where sso.status in ('1', '3', '4') and create_time BETWEEN #{start} and #{end}")
     public BigDecimal queryTotalFeeBetween(@Param("start") String start, @Param("end") String end);
 
     @Select("select count(1) from sys_sale_order sso where sso.status in ('1', '3', '4')")
@@ -38,13 +38,13 @@ public interface DashboardSaleViewMapper {
     @Select("select count(1) from sys_sale_order sso where sso.status in ('1', '3', '4') and create_time BETWEEN #{start} and #{end}")
     public int queryTotalTradeBetween(@Param("start") String start, @Param("end") String end);
 
-    @Select("SELECT a.app_id, sum( ssoi.actual_fee ) AS total_fee FROM sys_sale_order sso\n" +
+    @Select("SELECT a.app_id, IFNULL(sum( ssoi.actual_fee ),0) AS total_fee FROM sys_sale_order sso\n" +
             "\tJOIN sys_sale_order_item ssoi ON sso.order_id = ssoi.order_id AND ssoi.template_type = 1\n" +
             "\tJOIN sys_card_template ct ON ct.template_id = ssoi.template_id\n" +
             "\tJOIN sys_app a ON a.app_id = ct.app_id \n" +
             "WHERE sso.STATUS IN ( '1', '3', '4' )\n" +
             "GROUP BY a.app_id UNION ALL\n" +
-            "SELECT a.app_id, sum( ssoi.actual_fee ) AS total_fee FROM sys_sale_order sso\n" +
+            "SELECT a.app_id, IFNULL(sum( ssoi.actual_fee ),0) AS total_fee FROM sys_sale_order sso\n" +
             "\tJOIN sys_sale_order_item ssoi ON sso.order_id = ssoi.order_id AND ssoi.template_type = 2\n" +
             "\tJOIN sys_login_code_template ct ON ct.template_id = ssoi.template_id\n" +
             "\tJOIN sys_app a ON a.app_id = ct.app_id \n" +
@@ -52,13 +52,13 @@ public interface DashboardSaleViewMapper {
             "GROUP BY a.app_id")
     public List<Map<String, Object>> queryAppTotalFee();
 
-    @Select("SELECT a.app_id, sum( ssoi.actual_fee ) AS total_fee FROM sys_sale_order sso\n" +
+    @Select("SELECT a.app_id, IFNULL(sum( ssoi.actual_fee ),0) AS total_fee FROM sys_sale_order sso\n" +
             "\tJOIN sys_sale_order_item ssoi ON sso.order_id = ssoi.order_id AND ssoi.template_type = 1\n" +
             "\tJOIN sys_card_template ct ON ct.template_id = ssoi.template_id\n" +
             "\tJOIN sys_app a ON a.app_id = ct.app_id \n" +
             "WHERE sso.STATUS IN ( '1', '3', '4' ) AND sso.create_time BETWEEN #{start} AND #{end} \n" +
             "GROUP BY a.app_id UNION ALL\n" +
-            "SELECT a.app_id, sum( ssoi.actual_fee ) AS total_fee FROM sys_sale_order sso\n" +
+            "SELECT a.app_id, IFNULL(sum( ssoi.actual_fee ),0) AS total_fee FROM sys_sale_order sso\n" +
             "\tJOIN sys_sale_order_item ssoi ON sso.order_id = ssoi.order_id AND ssoi.template_type = 2\n" +
             "\tJOIN sys_login_code_template ct ON ct.template_id = ssoi.template_id\n" +
             "\tJOIN sys_app a ON a.app_id = ct.app_id \n" +
