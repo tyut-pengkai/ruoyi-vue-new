d7f7623d16e1e8a16b0b7e9cbf10cc64dc72861c
zwgu <1179403448@qq.com>
2023-04-06 19:33:14 +0800

    添加官网源码

--- vuepress-starter/docs/.vuepress/.temp/pages/guide/payment/payment.html.vue
+++
@@ -1,5 +0,0 @@
-<template><div><h1 id="支付宝当面付" tabindex="-1"><a aria-hidden="true" class="header-anchor" href="#支付宝当面付">#</a> 支付宝当面付</h1>
-<h3 id="_1111" tabindex="-1"><a aria-hidden="true" class="header-anchor" href="#_1111">#</a> 1111</h3>
-</div></template>
-
-
--- vuepress-starter/docs/.vuepress/.temp/pages/guide/qa.html.vue
+++
@@ -1,6 +0,0 @@
-<template><div><h1 id="常见问题" tabindex="-1"><a aria-hidden="true" class="header-anchor" href="#常见问题">#</a> 常见问题</h1>
-<h4 id="红叶是本地验证还是网络验证" tabindex="-1"><a aria-hidden="true" class="header-anchor" href="#红叶是本地验证还是网络验证">#</a> 红叶是本地验证还是网络验证？</h4>
-<p>网络验证，授权是本地，本地授权更稳定。</p>
-</div></template>
-
-
