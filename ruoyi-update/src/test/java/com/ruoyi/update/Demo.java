21917fbe517c7a53b2d25799d68040286e93960c
zwgu <1179403448@qq.com>
2023-06-19 14:10:55 +0800

    更新版本号

--- ruoyi-update/src/test/java/com/ruoyi/update/Demo.java
+++ ruoyi-update/src/test/java/com/ruoyi/update/Demo.java
@@ -37,8 +37,8 @@ public class Demo {
     @Test
     public void testGenUpdateInfoJson() {
         try {
-            String versionName = "v1.5.2";
-            Long versionNo = 20230330200000L;
+            String versionName = "v1.6.0";
+            Long versionNo = 20230620111900L;
             String fullVersion = versionName + "_" + versionNo;
             UpdateEngine ue = new UpdateEngine();
             ArrayList<FileInfo> updateList = ue.getFileInfoList("D:\\网络验证\\红叶\\release\\红叶网络验证系统_" + fullVersion + "\\upload");
