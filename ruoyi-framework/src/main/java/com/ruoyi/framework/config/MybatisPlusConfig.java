e7564cd3217f6183232aff79aa97bbac74c51f80
梁宇奇 <liangyuqi@bdfint.com>
2025-03-30 15:43:14 +0800

    fix:mybatis-plus分页

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/MybatisPlusConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/MybatisPlusConfig.java
@@ -2,6 +2,8 @@ package com.ruoyi.framework.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,20 +11,19 @@ import org.springframework.context.annotation.Configuration;
  * @author liangyq
  * @date 2025-03-18 15:16
  */
+@Slf4j
 @Configuration
 public class MybatisPlusConfig {
 
-    /**
-     * 乐观锁{@link com.baomidou.mybatisplus.annotation.Version}
-     *
-     * @return
-     */
+
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //乐观锁
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        //分页最后添加
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return interceptor;
     }
-
 
 }
