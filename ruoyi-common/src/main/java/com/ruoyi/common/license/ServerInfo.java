ace334c5e58ac35998411d4166c725af940a2481
zwgu <1179403448@qq.com>
2025-04-08 17:07:38 +0800

    feat(license): 实现服务器固定机器码功能
    - 新增固定机器码文件存储逻辑，支持 Windows 和 Linux 操作系统
    - 在服务器启动时检查固定机器码文件是否存在，若存在则解密并设置 SERVER_SN
    - 若固定机器码文件不存在，则生成新的机器码并加密存储到相应路径

--- ruoyi-common/src/main/java/com/ruoyi/common/license/ServerInfo.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/license/ServerInfo.java
@@ -1,41 +1,83 @@
 package com.ruoyi.common.license;
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.system.SystemUtil;
 import cn.hutool.system.oshi.OshiUtil;
 import com.ruoyi.common.constant.Constants;
+import com.ruoyi.common.utils.AesCbcPKCS5PaddingUtil;
 import com.ruoyi.common.utils.ip.IpUtils;
 import com.ruoyi.common.utils.sign.Md5Utils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import oshi.hardware.NetworkIF;
 
+import java.io.File;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+@Slf4j
 public class ServerInfo {
 
     public static void getServerInfo() {
+        String hostIp = ServerInfo.getServerIp();
+        Constants.IP_ADDRESS = Collections.singletonList(hostIp);
         if (StringUtils.isBlank(Constants.SERVER_SN)) {
-            String hostIp = ServerInfo.getServerIp();
-            String macAddress = null;
-            List<NetworkIF> networkIFs = OshiUtil.getNetworkIFs();
-            for (NetworkIF n : networkIFs) {
-                if (Arrays.asList(n.getIPv4addr()).contains(hostIp)) {
-                    macAddress = n.getMacaddr();
-                    break;
-                }
+            // 判断固定机器码文件是否存在
+            String storePath = "";
+            String fileName = Md5Utils.hash(SystemUtil.getUserInfo().getCurrentDir()).toLowerCase();
+            if (SystemUtil.getOsInfo().getName().toLowerCase().contains("windows") || SystemUtil.getOsInfo().isWindows()) {
+                storePath = "C:\\ProgramData\\hywlyz\\" + fileName;
+            } else if (SystemUtil.getOsInfo().getName().toLowerCase().contains("linux") || SystemUtil.getOsInfo().isLinux()) {
+                storePath = "/var/lib/hywlyz/" + fileName;
+            } else {
+                getServerSn(null);
+                log.error("未知操作系统：{}，固定机器码失败", SystemUtil.getOsInfo().getName());
+                return;
             }
-            //序列号
-            String serialNumber = OshiUtil.getSystem().getSerialNumber();
-            //处理器ID
-            String processorID = OshiUtil.getProcessor().getProcessorIdentifier().getProcessorID();
-            //组装 系统机器码 mac串+序列号+处理器ID+程序系统路径+系统名称+主机名+系统架构+环境版本号  -->机器码  可以自行增加硬件信息确保唯一性
-            String temp = "CORE|" + macAddress + serialNumber + processorID
-                    + SystemUtil.getUserInfo().getCurrentDir() + SystemUtil.getOsInfo().getName()
-                    + SystemUtil.getHostInfo().getName() + SystemUtil.getOsInfo().getArch()
-                    + SystemUtil.getJavaInfo().getVersion();
-            //再将机器码加密成16位字符串
-            Constants.SERVER_SN = com.ruoyi.common.utils.StringUtils.insertPer4Char(Md5Utils.hash(temp).toUpperCase());
-            Constants.IP_ADDRESS = Collections.singletonList(hostIp);
+            File file = new File(storePath);
+            if(file.exists() && file.isFile()) {
+                String s = FileUtil.readString(storePath, StandardCharsets.UTF_8);
+                try {
+                    Constants.SERVER_SN = AesCbcPKCS5PaddingUtil.decode(s, Constants.STORE_PASS);
+                } catch (Exception e) {
+                    log.error("固定机器码获取失败", e);
+                    getServerSn(storePath);
+                }
+            } else {
+                getServerSn(storePath);
+            }
+        }
+    }
+
+    private static void getServerSn(String storePath) {
+        String hostIp = ServerInfo.getServerIp();
+        String macAddress = null;
+        List<NetworkIF> networkIFs = OshiUtil.getNetworkIFs();
+        for (NetworkIF n : networkIFs) {
+            if (Arrays.asList(n.getIPv4addr()).contains(hostIp)) {
+                macAddress = n.getMacaddr();
+                break;
+            }
+        }
+        //序列号
+        String serialNumber = OshiUtil.getSystem().getSerialNumber();
+        //处理器ID
+        String processorID = OshiUtil.getProcessor().getProcessorIdentifier().getProcessorID();
+        //组装 系统机器码 mac串+序列号+处理器ID+程序系统路径+系统名称+主机名+系统架构+环境版本号  -->机器码  可以自行增加硬件信息确保唯一性
+        String temp = "CORE|" + macAddress + serialNumber + processorID
+                + SystemUtil.getUserInfo().getCurrentDir() + SystemUtil.getOsInfo().getName()
+                + SystemUtil.getHostInfo().getName() + SystemUtil.getOsInfo().getArch()
+                + SystemUtil.getJavaInfo().getVersion();
+        //再将机器码加密成16位字符串
+        Constants.SERVER_SN = com.ruoyi.common.utils.StringUtils.insertPer4Char(Md5Utils.hash(temp).toUpperCase());
+        try {
+            // 固定机器码，避免重启服务器后机器码变化
+            if (StringUtils.isNotBlank(storePath)) {
+                FileUtil.writeString(AesCbcPKCS5PaddingUtil.encode(Constants.SERVER_SN, Constants.STORE_PASS), storePath, StandardCharsets.UTF_8);
+            }
+        } catch (Exception e) {
+            log.error("固定机器码失败", e);
         }
     }
 
