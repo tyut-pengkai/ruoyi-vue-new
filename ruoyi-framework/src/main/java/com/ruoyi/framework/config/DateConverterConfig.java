3e22b867475389ba6ec9e9b0431b46c097e56384
梁宇奇 <liangyuqi@bdfint.com>
2025-05-13 00:56:38 +0800

    fix

---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/DateConverterConfig.java
@@ -0,0 +1,62 @@
+package com.ruoyi.framework.config;
+
+import cn.hutool.core.date.DateUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @author liangyq
+ * @date 2025-05-13
+ */
+@Configuration
+public class DateConverterConfig {
+
+    @Bean
+    public Converter<String, Date> dateConverter() {
+        return new Converter<String, Date>() {
+            @Override
+            public Date convert(String source) {
+                return DateUtil.parse(source);
+            }
+        };
+    }
+
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+        //Date序列化和反序列化
+        javaTimeModule.addSerializer(Date.class, new JsonSerializer<Date>() {
+            @Override
+            public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+                    throws IOException {
+                jsonGenerator.writeString(DateUtil.formatDateTime(date));
+            }
+        });
+        javaTimeModule.addDeserializer(Date.class, new JsonDeserializer<Date>() {
+            @Override
+            public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+                    throws IOException {
+                return DateUtil.parse(jsonParser.getText());
+            }
+        });
+
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
+    }
+
+
+}
