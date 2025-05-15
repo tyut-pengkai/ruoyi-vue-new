b6cf104057fd2df4a859f1e61e2b25c99f5b7258
czw <2254442311@qq.com>
2025-05-15 20:11:11 +0800

    feat(generator): 为代码生成器添加 Swagger 注解
    - 在控制器和领域对象上添加了 Swagger 注解
    - 更新了 GenConstants 类，添加了新的数据类型常量
    - 调整了 GenUtils 类中的数据类型处理逻辑- 更新了 generator.yml 文件中的作者信息

--- zzyl-nursing-platform/src/main/java/com/zzyl/nursing/domain/NursingProject.java
+++ zzyl-nursing-platform/src/main/java/com/zzyl/nursing/domain/NursingProject.java
@@ -2,6 +2,8 @@ package com.zzyl.nursing.domain;
 
 import com.zzyl.common.annotation.Excel;
 import com.zzyl.common.core.domain.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,54 +19,63 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("护理项目对象")
 public class NursingProject extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
     /**
      * 编号
      */
+    @ApiModelProperty("编号")
     private Long id;
 
     /**
      * 名称
      */
     @Excel(name = "名称")
+    @ApiModelProperty("名称")
     private String name;
 
     /**
      * 排序号
      */
     @Excel(name = "排序号")
+    @ApiModelProperty("排序号")
     private Integer orderNo;
 
     /**
      * 单位
      */
     @Excel(name = "单位")
+    @ApiModelProperty("单位")
     private String unit;
 
     /**
      * 价格
      */
     @Excel(name = "价格")
+    @ApiModelProperty("价格")
     private BigDecimal price;
 
     /**
      * 图片
      */
     @Excel(name = "图片")
+    @ApiModelProperty("图片")
     private String image;
 
     /**
      * 护理要求
      */
     @Excel(name = "护理要求")
+    @ApiModelProperty("护理要求")
     private String nursingRequirement;
 
     /**
      * 状态
      */
     @Excel(name = "状态")
+    @ApiModelProperty("状态")
     private Integer status;
 
 
