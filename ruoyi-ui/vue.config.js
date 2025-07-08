0b212dedd1938fbfa8b9ec8cc0e7bd709be42b19
盘友辉 <panyouhui@ule.com>
2025-07-08 13:09:20 +0800

    页面功能模块

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -12,7 +12,7 @@ const name = process.env.VUE_APP_TITLE || '若依管理系统' // 网页标题
 //const baseUrl = 'http://localhost:8080' // 后端接口
 const baseUrl = 'http://121.40.218.168:18088' // 后端接口
 
-const port = process.env.port || process.env.npm_config_port || 18088 // 端口
+const port = process.env.port || process.env.npm_config_port || 80 // 端口
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
